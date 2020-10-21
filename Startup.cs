28946d4c41cd2a6386f657376443463e09862557
dan su <danchfr@gmail.com>
2020-10-21 15:25:39 -0400

    testRevert

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,8 @@ namespace WebAPI
             services.AddDbContext<PaymentDetailContext>(options =>
             options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
 
+           //testRevert
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
